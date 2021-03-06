ad1b29a34d5d3c768cbb68692672b76f81cb362b
yeridi <yeridicrhis2002@gmail.com>
2021-03-06 13:35:43 -0500

    finish

---
+++ README.md
@@ -0,0 +1,1 @@
+https://react-three-minecraft.netlify.app/
---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, {Suspense, useState} from "react";
+//Styles
+import "./assets/styles/App.scss";
+//Three
+import {Canvas, useThree} from 'react-three-fiber'
+import Lights from './components/Three/lights'
+import Floor from './components/Three/floor'
+import {softShadows, Loader, OrbitControls} from '@react-three/drei'
+//Model
+import Model from './components/Three/chest'
+import {useSpring} from 'react-spring'
+
+
+softShadows();
+
+//zoom efect on load
+
+const ZoomWithOrbital = () =>{
+  const {gl, camera} = useThree()
+  useSpring({
+    from:{
+      z:30
+    },
+    x: -5,
+    y: 4,
+    z:4,
+    onFrame:({x,y,z}) => {
+      camera.position.x = x;
+      camera.position.y = y;
+      camera.position.z = z;
+    } 
+  })
+  return (
+    <OrbitControls 
+      enableZoom={false}
+      enablePan={false}
+      target={[0,0,0]}
+      args={[camera,gl.domElement]}
+    />
+  )
+}
+
+const App = () => {
+
+  const [open,setOpen] = useState(false)
+
+
+  return (
+    <>
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{position:[-5,4,4], fov:40}}
+      >
+        <Lights/>
+        <Suspense fallback={null}>
+          <Model open={open} setOpen={setOpen}></Model>
+          <Floor></Floor>
+          <ZoomWithOrbital />
+        </Suspense>
+      </Canvas>
+      {/* Cargador */}
+      <Loader/>
+    </>
+  );
+};
+
+export default App;
